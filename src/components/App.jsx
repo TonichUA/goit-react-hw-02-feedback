9579ce9a6211a28d9da0e036a9305588bf9e9695
TonichUA <132302438+TonichUA@users.noreply.github.com>
2023-11-15 00:38:18 +0200

    correcting  mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,30 @@
-import { FeedbackCounter } from '../components/Feedback';
+import React, { useState } from 'react';
+import { Feedback } from '../components/Feedback';
 import { Section } from './Section';
+
 export const App = () => {
+  const [feedbackState, setFeedbackState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const onClickFeedback = option => {
+    setFeedbackState(prevState => ({
+      ...prevState,
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    return feedbackState.good + feedbackState.neutral + feedbackState.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total > 0 ? Math.round((feedbackState.good / total) * 100) : 0;
+  };
+
   return (
     <div
       style={{
@@ -13,7 +37,12 @@ export const App = () => {
       }}
     >
       <Section title="Please leave feedback">
-        <FeedbackCounter />
+        <Feedback
+          feedbackState={feedbackState}
+          onClickFeedback={onClickFeedback}
+          countTotalFeedback={countTotalFeedback}
+          countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+        />
       </Section>
     </div>
   );
